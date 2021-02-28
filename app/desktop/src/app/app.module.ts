c6fcbc1a735749535a2c1a0a18bfba3ba48a1990
Petr Hanák <jsem@petr-hanak.cz>
2021-02-28 21:59:32 +0100

    feat: desktop feature counter

--- app/desktop/src/app/app.module.ts
+++ app/desktop/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeatureModule } from '@ngca/feature'
 
 @NgModule({
   declarations: [
@@ -11,8 +12,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FeatureModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- lib/feature/src/lib/feature.component.ts
+++ lib/feature/src/lib/feature.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { interval } from 'rxjs'
 
 @Component({
   selector: 'lib-feature',
   template: `
     <p>
       feature works!
+      {{count}}
     </p>
   `,
   styles: [
   ]
 })
 export class FeatureComponent implements OnInit {
+  count: number = 0
 
   constructor() { }
 
   ngOnInit(): void {
+    const counter = interval(1000);
+
+    counter.subscribe(num => this.count = num)
   }
 
 }
