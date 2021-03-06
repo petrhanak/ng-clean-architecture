3d5eb5c572b47587fe4d8cd28137f2df129b6fb9
Petr Hanák <jsem@petr-hanak.cz>
2021-03-06 12:13:17 +0100

    feat: absolute imports with dependency context

---
+++ modules/main/feature/src/example/another.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'lib-another',
+  template: `
+    <button>click me</button>
+  `,
+  styles: [],
+})
+export class AnotherComponent {
+}
--- modules/main/feature/src/example/example.module.ts
+++ modules/main/feature/src/example/example.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { UiModule } from '@ngca/module/main/core'
+import { AnotherComponent } from '@ngca/module/main/feature/example/another.component'
 import { ExampleComponent } from '@ngca/module/main/feature/example/example.component'
 
 @NgModule({
-  declarations: [ExampleComponent],
+  declarations: [ExampleComponent, AnotherComponent],
   imports: [UiModule],
-  exports: [ExampleComponent],
+  exports: [ExampleComponent, AnotherComponent],
 })
 export class ExampleModule {}
